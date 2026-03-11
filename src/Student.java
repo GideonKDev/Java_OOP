876770f460ef2b9ea0c70a83e5d861138c7aae2f
GideonKDev <kiruigideon2006@gmail.com>
2026-03-11 18:45:53 +0300

    introduction to Loops

--- src/Student.java
+++ src/Student.java
@@ -1,14 +1,37 @@
+
+import java.util.Scanner;
+import javax.naming.event.NamingExceptionEvent;
 public class Student {
+  /*
+  Problem 2: Student Class
+        Create a Student class with:
+        Private fields: name, rollNumber, marks (in 3 subjects)
+        Constructor to initialize all fields
+        Method calculatePercentage() returns average
+        Method displayGrade() returns 'A' (>80%), 'B' (60-80%), 'C' (40-60%), 'F' (<40%) */
     private String name;
-    private int age;
+    private String rollNumber;
+    private int[] marks = new int[3];
+    private int total=0;
 
-    //constructor
-    public Student(String name, int age){
-        this.name = name;
-        this.age = age;
+    public Student(String name, String rollNumber, int[] marks) {
+    this.name = name;
+    this.rollNumber = rollNumber;
+    this.marks = marks; // This assigns the array passed in to your private field
     }
 
-    public void displayDetails(){
-        System.out.println("Hello "+name+" You are "+age+" Years Old");
+  public double calculatePercentage() {
+        int total = 0;
+        for (int m : marks) { // This is a "for-each" loop in Java, very clean!
+            total += m;
+        }
+        return total / 3.0; // Returns the average
     }
+
+    public String displayGrade(){
+        
+    }
+
+
+
 }
