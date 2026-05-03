41107f17d1c851a26fc1adc5637f13893d0df40b
GideonKDev <kiruigideon2006@gmail.com>
2026-05-03 22:16:46 +0300

    Assignment two: Java collection framework

---
+++ Assignment/Student.java
@@ -0,0 +1,35 @@
+/**
+ * @author Master Kirui
+ */
+
+package Java_OOP.Assignment;
+
+public class Student {
+    private int id;
+    private String name;
+    private String mail;
+
+    public void addStudent(){
+
+    }
+
+    public void enrollCourse(){
+
+    }
+
+    public void recordGrade(){
+
+    }
+
+    public void generateReport(){
+
+    }
+    
+    public void search(){
+
+    }
+
+    public void calculate(){
+        
+    }
+}
