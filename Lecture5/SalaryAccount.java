a484fb0d7c3e73e8b98e5f97bfa530c7924dd20d
GideonKDev <kiruigideon2006@gmail.com>
2026-05-12 11:51:13 +0300

    Java interfaces

---
+++ Lecture5/SalaryAccount.java
@@ -0,0 +1,5 @@
+package Java_OOP.Lecture5;
+
+public class SalaryAccount {
+
+}
---
+++ Lecture5_Interfaces/Interface.java
@@ -0,0 +1,21 @@
+package Java_OOP.Lecture5_Interfaces;
+
+public interface  Interface{
+    public String NAME = "Master";
+    // public void methodDemo(){};
+    public abstract void Demo(); // correct way to create methods 
+    int demoMethod(){}; //error
+
+
+
+
+}
+//interface can extend each other
+interface Demo extends Interface{
+    String Demo_function();
+}
+
+
+class DemoClass implements Interface{
+
+}
