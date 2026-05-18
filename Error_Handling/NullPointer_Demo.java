91b887731612e712fe7813f76992614f5362d62b
GideonKDev <kiruigideon2006@gmail.com>
2026-05-18 11:09:30 +0300

    Null pointer exeption

--- Error_Handling/NullPointer_Demo.java
+++ Error_Handling/NullPointer_Demo.java
@@ -1,5 +1,12 @@
 package Java_OOP.Error_Handling;
 
 public class NullPointer_Demo {
-    
+    public static void main(String[] args) {
+        try{
+            String a = null;// a  is null therefore returns an error by trying to display character at a given index
+            System.out.println(a.charAt(0));
+        }catch(NullPointerException e){
+                System.out.println(e.getMessage());
+        }
+    }
 }
