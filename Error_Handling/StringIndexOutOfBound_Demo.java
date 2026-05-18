bff0f43e844f0ed4bbb99941c49c49ca1b361a44
GideonKDev <kiruigideon2006@gmail.com>
2026-05-18 11:12:57 +0300

    Array Index Out of Bounds

--- Error_Handling/StringIndexOutOfBound_Demo.java
+++ Error_Handling/StringIndexOutOfBound_Demo.java
@@ -1,5 +1,17 @@
 package Java_OOP.Error_Handling;
 
 public class StringIndexOutOfBound_Demo {
-    
+    public static void main(String[] args) {
+
+        try {
+            String a = "Vehicle";
+            char c = a.charAt(8);
+            System.out.println(c);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+            
+
+    }
 }
