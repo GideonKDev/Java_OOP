18d454a365256eab6fd36787b75895aa3d85a782
GideonKDev <kiruigideon2006@gmail.com>
2026-05-19 11:25:29 +0300

    Java Dialogue Boxes

--- JavaGUI/DialogueBoxes.java
+++ JavaGUI/DialogueBoxes.java
@@ -4,5 +4,19 @@ import javax.swing.JOptionPane;
 public class DialogueBoxes {
     public static void main(String[] args){
         JOptionPane.showMessageDialog(null,"Welcome to Java GUI");
+
+        //Input names and calculate age
+        String name = JOptionPane.showInputDialog("Enter Name: ");
+        int year = Integer.parseInt(JOptionPane.showInputDialog(null,name+" When Were you Born","Year Of Birth", JOptionPane.WARNING_MESSAGE));
+
+        //take in the current year of Birth:
+        int currentYear = Integer.parseInt(JOptionPane.showInputDialog("Current Year: "));
+
+        int age = currentYear - year;
+        JOptionPane.showMessageDialog(null,"Age: "+age);
+
+        //Developing a dropDown
+        
+
     }
 }
