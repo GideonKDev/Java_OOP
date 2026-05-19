b468258c383561872e64836e3e644e1755e05de2
GideonKDev <kiruigideon2006@gmail.com>
2026-05-19 11:16:48 +0300

    Java GUI

---
+++ JavaGUI/DialogueBoxes.java
@@ -0,0 +1,8 @@
+package Java_OOP.JavaGUI;
+import javax.swing.JOptionPane;
+
+public class DialogueBoxes {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null,"Welcome to Java GUI");
+    }
+}
