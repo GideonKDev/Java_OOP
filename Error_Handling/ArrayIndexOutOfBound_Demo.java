a31a1d8ca90977409534369d4460014a88cbb2bb
GideonKDev <kiruigideon2006@gmail.com>
2026-05-24 12:58:53 +0300

    Array Index Out of Bonds

--- Error_Handling/ArrayIndexOutOfBound_Demo.java
+++ Error_Handling/ArrayIndexOutOfBound_Demo.java
@@ -1,5 +1,26 @@
 package Java_OOP.Error_Handling;
 
+import java.util.Scanner;
+
 public class ArrayIndexOutOfBound_Demo {
-    
+    public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+    int[] numbers = {10,20,30,40,50};
+
+    try{
+        System.out.println("Enter index to check for number: ");
+        int i = input.nextInt();
+
+        int num = numbers[i];
+        System.out.println("Number at index "+i+" is "+num);
+
+    }catch(ArrayIndexOutOfBoundsException e){
+        System.out.println("The array is not that Longer :haha");
+        System.out.println("This is the error "+e.getMessage());
+    }
+    finally{
+        System.out.println("Error Handled successfully");
+    }     
+    }
+  
 }
